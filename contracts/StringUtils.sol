42a725362f8cf356c840b1f83512070f138e5505
Mesi Rendon <amrendonsa@unal.edu.co>
2018-10-25 18:00:53 -0500

    Adding StringUtil library
            Since the new version of Web3 is not capable of implicit
            conversions between `string`s and `bytes32`, we had to implement
            our own version of explicit conversions for `string`s.
    
            Reported-by: Mesi Rendon

---
+++ contracts/StringUtils.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+
+library StringUtils {
+
+  function stringToBytes32(string memory source) pure internal returns (bytes32 result) {
+    assembly {
+      result := mload(add(source, 32))
+    }
+  }
+}
--- contracts/Toolkit.sol
+++ contracts/Toolkit.sol
@@ -1,16 +1,22 @@
 pragma solidity ^0.4.23;
 
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import './StringUtils.sol';
 
 contract Toolkit is Ownable {
+  using StringUtils for string;
   mapping(bytes32 => mapping(bytes32 => string)) hashes;
 
-  function add(bytes32 _part1, bytes32 _part2, string _name) public onlyOwner {
-    hashes[_part1][_part2] = _name;
+  function add(string _part1, string _part2, string _name) public onlyOwner {
+    bytes32 part1 = _part1.stringToBytes32();
+    bytes32 part2 = _part2.stringToBytes32();
+    hashes[part1][part2] = _name;
   }
 
-  function validate(bytes32 _part1, bytes32 _part2) public view onlyOwner returns (string result)  {
-    result = hashes[_part1][_part2];
+  function validate(string _part1, string _part2) public view onlyOwner returns (string result)  {
+    bytes32 part1 = _part1.stringToBytes32();
+    bytes32 part2 = _part2.stringToBytes32();
+    result = hashes[part1][part2];
   }
 
   function kill() public onlyOwner {
